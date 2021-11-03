65918898672a14bf55977dffe9037fbda1c957c2
Amber Caldwell <acaldwell710@gmail.com>
2021-11-02 20:54:00 -0400

    added controllers, routes, and updated hbs

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
 const roomsRouter = require('./app_server/routes/rooms');
 const mealsRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
+const aboutRouter = require('./app_server/routes/about');
+const contactRouter = require('./app_server/routes/contact');
 const { hasSubscribers } = require('diagnostics_channel');
 
 const app = express();
@@ -32,6 +35,9 @@ app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/rooms', roomsRouter);
 app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app_server/controllers/news.js
@@ -0,0 +1,7 @@
+const news = (req, res) => {
+    res.render('news', {title: 'Travlr Getaway' });
+};
+
+module.exports = {
+    news
+};
